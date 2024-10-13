80cb1f329de6e19584e5d35160993c34a75ee4d5
Orochii Zouveleki <arcboundcrusher@gmail.com>
2024-10-12 20:12:06 -0600

    Updates to inventory slots.

--- Source/UI/InvSlotButton.cs
+++ Source/UI/InvSlotButton.cs
@@ -3,29 +3,25 @@ using System;
 
 public partial class InvSlotButton : TextureButton
 {
+	[Export] Control Container;
 	[Export] TextureRect Icon;
 	[Export] Label amount;
 	GameState.ItemEntry CurrentEntry;
 	public BaseItem Item = null;
-	public void Setup(GameState.ItemEntry entry, bool setAmount=false, bool ignoreVisuals=false) {
+	public void Setup(GameState.ItemEntry entry, bool ignoreVisuals=false) {
 		CurrentEntry = entry;
-		if (entry == null) {
+		Item = (entry==null) ? null : BaseItem.Get(entry.itemID);
+		if (entry == null || ignoreVisuals) {
 			Icon.Texture = null;
-			Item = null;
+			Container.Visible = false;
 		} else {
-			Item = BaseItem.Get(entry.itemID);
-			if (!ignoreVisuals) {
-				Icon.Texture = Item.Icon;
-				float sizeX = Math.Max(32, Item.SlotSize.X * 32);
-				float sizeY = Math.Max(32, Item.SlotSize.Y * 32);
-				Icon.Size = new Vector2(sizeX, sizeY);
-			} else {
-				Icon.Texture = null;
-			}
-		}
-		amount.Text = "";
-		if (setAmount) {
-			if (entry.stackSize > 1) amount.Text = entry.stackSize.ToString();
+			Icon.Texture = Item.Icon;
+			float sizeX = Math.Max(32, Item.SlotSize.X * 32);
+			float sizeY = Math.Max(32, Item.SlotSize.Y * 32);
+			Container.Size = new Vector2(sizeX, sizeY);
+			Container.Visible = true;
+			// if (entry.stackSize > 1)
+			amount.Text = entry.stackSize.ToString();
 		}
 	}
 }
