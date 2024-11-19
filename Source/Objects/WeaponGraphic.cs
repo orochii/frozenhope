b711be5817ed515e4812d4e01157467432d51396
OzzyTheOne <calvetosi@gmail.com>
2024-11-19 15:46:05 -0300

    Imported Sounf Effects
    Imported all the sound effects that Davve (JDY) has made so far.

--- Source/Objects/WeaponGraphic.cs
+++ Source/Objects/WeaponGraphic.cs
@@ -5,6 +5,7 @@ public partial class WeaponGraphic : Node3D
 {
 	[Export] AnimationPlayer Animator;
 	[Export] public AudioStreamPlayer SoundEffect;
+	[Export] public AudioStreamPlayer Reload;
 	[Export] public Node3D SpawnPoint;
 	public void Play(string animation) {
 		if (Animator != null) Animator.Play(animation);
